a50727d0ecb3265532d951ce4297f0638d4ee0fc
izzylppz <https://github.com/Shmizzy>
2024-06-06 15:05:39 -0500

    init

---
+++ locations_project/locations_api/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+from .serializers import LocationSerializer
+from .models import Location
+
+# Create your views here.
+class LocationList(generics.ListCreateAPIView):
+    queryset = Location.objects.all().order_by('id')
+    serializer_class = LocationSerializer
+
+class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Location.objects.all().order_by('id')
+    serializer_class = LocationSerializer
